a4a204de1373ce123856f4c5383752fc420d34ba
Puff14 <100191494+Puff14@users.noreply.github.com>
2023-10-02 11:16:52 +0200

    Start

---
+++ Arkadehalspil/Assets/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void Takedamage(int damageamount)
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
